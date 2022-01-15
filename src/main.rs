08c864ddc7beaf76e6750a3550bb28d8f9dbaecd
Stephan Kern <stephan@swmmrman.com>
2022-01-15 15:37:45 -0700

    First Test of calling g910-led

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    const BACKGROUND: &str = "562500";
+    const START_COLOR: &str = "210000";
+    let output1 = Command::new("g910-led").args(["-a", START_COLOR]).output().expect("Failed to execute");
+    let output2 = Command::new("g910-led").args(["-a", BACKGROUND]).output().expect("Failed to execute");
+
+    println!("{:?}\n{:?}", output1, output2);
 }
