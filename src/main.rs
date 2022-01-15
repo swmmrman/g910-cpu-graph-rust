2a3ebb0ec4d05fae505a076388d60f2a5c61e8f0
Stephan Kern <stephan@swmmrman.com>
2022-01-15 15:40:58 -0700

    Add A 1 second sleep

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::process::Command;
+use std::{thread, time};
 
 fn main() {
     const BACKGROUND: &str = "562500";
     const START_COLOR: &str = "210000";
     let output1 = Command::new("g910-led").args(["-a", START_COLOR]).output().expect("Failed to execute");
+    thread::sleep(time::Duration::from_secs(1));
     let output2 = Command::new("g910-led").args(["-a", BACKGROUND]).output().expect("Failed to execute");
 
     println!("{:?}\n{:?}", output1, output2);
