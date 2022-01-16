709551b4631f6180dfc309f2a2bbb3b65209683f
Stephan Kern <stephan@swmmrman.com>
2022-01-15 18:25:46 -0700

    Set whole keyboard by cpu0

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use sysinfo::{System, SystemExt, ProcessorExt};
 use std::process::Command;
 use std::{thread, time};
+use std::fs::File;
+use std::io::Write;
 
 
 fn get_color(percent: f32) -> String {
@@ -10,16 +12,22 @@ fn get_color(percent: f32) -> String {
     format!("{:02X}{:02X}00", r, g)
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let mut sys = System::new_all();
     sys.refresh_all();
-    const BACKGROUND: &str = "562500";
+    // const BACKGROUND: &str = "562500";
     const START_COLOR: &str = "210000";
     let output1 = Command::new("g910-led").args(["-a", START_COLOR]).output().expect("Failed to execute");
     thread::sleep(time::Duration::from_secs(1));
-    let output2 = Command::new("g910-led").args(["-a", BACKGROUND]).output().expect("Failed to execute");
     for core in sys.processors() {
         println!("Core: {}, Frequency: {}mhz, Usage: {:0.2}, Color {}", core.name(), core.frequency(), core.cpu_usage(), get_color(core.cpu_usage()));
+        if core.name() == "cpu0" {
+            let mut file = File::create("/tmp/g910.profile")?;
+            let command = format!("a {}\nc", get_color(core.cpu_usage()));
+            file.write_all(command.as_bytes())?;
+        }
     }
+    let output2 = Command::new("g910-led").args(["-p", "/tmp/g910.profile"]).output().expect("Failed to execute");
     println!("{:?}\n{:?}", output1, output2);
+    Ok(())
 }
