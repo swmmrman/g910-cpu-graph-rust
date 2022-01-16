2667517ba35c40876ed890dde6b86e433f426875
Stephan Kern <stephan@swmmrman.com>
2022-01-15 17:19:38 -0700

    Create get color function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ use sysinfo::{System, SystemExt, ProcessorExt};
 use std::process::Command;
 use std::{thread, time};
 
+
+fn get_color(percent: f32) -> String {
+    let percent = percent / 100.0;
+    let r = (percent * 255.0) as i32;
+    let g =  (110.0 - (percent * 110.0)) as i32;
+    format!("{:02X}{:02X}00", r, g)
+}
+
 fn main() {
     let mut sys = System::new_all();
     sys.refresh_all();
@@ -11,7 +19,7 @@ fn main() {
     thread::sleep(time::Duration::from_secs(1));
     let output2 = Command::new("g910-led").args(["-a", BACKGROUND]).output().expect("Failed to execute");
     for core in sys.processors() {
-        println!("Core: {}, Frequency: {}mhz, Usage: {:0.2}", core.name(), core.frequency(), core.cpu_usage());
+        println!("Core: {}, Frequency: {}mhz, Usage: {:0.2}, Color {}", core.name(), core.frequency(), core.cpu_usage(), get_color(core.cpu_usage()));
     }
     println!("{:?}\n{:?}", output1, output2);
 }
