7a917763f9783fe9dd8623866eff158b925809d6
Stephan Kern <stephan@swmmrman.com>
2022-01-16 01:44:55 -0700

    Add CPU temp to logo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use sysinfo::{System, SystemExt, ProcessorExt};
 use std::process::Command;
-use std::{thread, time};
+use std::{thread, time, fs};
 use std::fs::File;
 use std::io::Write;
 
@@ -10,6 +10,13 @@ fn get_color(percent: f32) -> String {
     let r = (percent * 255.0) as i32;
     let g =  (110.0 - (percent * 110.0)) as i32;
     format!("{:02X}{:02X}00", r, g)
+}
+
+fn get_cpu_temp() -> f32 {
+    let temp: String = fs::read_to_string("/sys/class/thermal/thermal_zone1/temp")
+        .expect("File missing");
+    let temp = temp.trim().parse::<f32>().unwrap();
+    temp / 1000.0
 }
 
 fn main() -> std::io::Result<()> {
@@ -30,10 +37,13 @@ fn main() -> std::io::Result<()> {
             command.push_str(&newpart);
             counter += 1;
         }
+        let cpu_temp = get_cpu_temp();
+        let temp_str = format!("\ng logo {}", get_color((cpu_temp-30.0)*1.4));
+        command.push_str(&temp_str);
         command.push_str("\nc");
         file.write_all(command.as_bytes())?;
         let _output2 = Command::new("g910-led").args(["-p", "/tmp/g910.profile"]).output().expect("Failed to execute");
-        thread::sleep(time::Duration::from_millis(100));
+        thread::sleep(time::Duration::from_millis(500));
     }
     Ok(())
 }
