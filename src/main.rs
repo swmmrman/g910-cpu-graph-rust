b178d21871be2389679ba235edf30d0b6217ec8f
Stephan Kern <stephan@swmmrman.com>
2022-01-15 18:35:27 -0700

    Strip unneded debug messages

--- src/main.rs
+++ src/main.rs
@@ -18,17 +18,15 @@ fn main() -> std::io::Result<()> {
     sys.refresh_all();
     // const BACKGROUND: &str = "562500";
     const START_COLOR: &str = "210000";
-    let output1 = Command::new("g910-led").args(["-a", START_COLOR]).output().expect("Failed to execute");
+    let _output1 = Command::new("g910-led").args(["-a", START_COLOR]).output().expect("Failed to execute");
     thread::sleep(time::Duration::from_secs(1));
     for core in sys.processors() {
-        println!("Core: {}, Frequency: {}mhz, Usage: {:0.2}, Color {}", core.name(), core.frequency(), core.cpu_usage(), get_color(core.cpu_usage()));
         if core.name() == "cpu0" {
             let mut file = File::create("/tmp/g910.profile")?;
             let command = format!("a {}\nc", get_color(core.cpu_usage()));
             file.write_all(command.as_bytes())?;
         }
     }
-    let output2 = Command::new("g910-led").args(["-p", "/tmp/g910.profile"]).output().expect("Failed to execute");
-    println!("{:?}\n{:?}", output1, output2);
+    let _output2 = Command::new("g910-led").args(["-p", "/tmp/g910.profile"]).output().expect("Failed to execute");
     Ok(())
 }
